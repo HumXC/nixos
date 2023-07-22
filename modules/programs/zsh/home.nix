fcb06c35ce9491572eec1c974cf28322dc38864b
HumXC <Hum-XC@outlook.com>
2023-07-23 05:58:11 +0800

    zsh 增加插件

--- modules/programs/zsh/home.nix
+++ modules/programs/zsh/home.nix
@@ -35,7 +35,9 @@
     zplug = {
       enable = true;
       plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+        { name = "zsh-users/zsh-autosuggestions"; }
+        { name = "zsh-users/zsh-completions"; }
+        { name = "zsh-users/zsh-syntax-highlighting"; }
         { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
       ];
     };
