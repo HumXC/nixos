2ed2c384c91f415982c98f10fb9dad92e78b9500
HumXC <Hum-XC@outlook.com>
2023-07-23 08:22:16 +0800

    修改四叶草输入方案的安装方式

--- modules/programs/fcitx5/home.nix
+++ modules/programs/fcitx5/home.nix
@@ -3,8 +3,8 @@ let
   # See: https://github.com/fkxxyz/rime-cloverpinyin/wiki/linux#%E5%AE%89%E8%A3%85%E8%AF%A5%E8%BE%93%E5%85%A5%E6%96%B9%E6%A1%88
   version = "1.1.4";
   clover-schema = pkgs.fetchzip {
-    url = "https://github.com/fkxxyz/rime-cloverpinyin/releases/download/${version}/clover.schema-build-${version}.zip";
-    sha256 = "sha256-TMiWdSsK1G1sfJgMDJn7iXIjEuIdVwAKKCXQeWHAeQ8=";
+    url = "https://github.com/fkxxyz/rime-cloverpinyin/releases/download/${version}/clover.schema-${version}.zip";
+    sha256 = "sha256-34nPX5RQujAHNJmY0GOV0PjgrYOmC0aTO12XGtTrQKQ=";
     stripRoot = false;
   };
 
