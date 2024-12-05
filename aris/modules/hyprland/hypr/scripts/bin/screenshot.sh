77494a9dc0dc58911598dec878a0b58ad2c5c341
HumXC <Hum-XC@outlook.com>
2024-12-06 00:14:25 +0800

    fix

--- aris/modules/hyprland/hypr/scripts/bin/screenshot.sh
+++ aris/modules/hyprland/hypr/scripts/bin/screenshot.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-DIR="$HOME/Pictures/screenshot"
+DIR="$HOME/Pictures/Screenshot"
 if [ ! -d "$DIR" ]; then
     mkdir -p "$DIR"
 fi
 # ags request "screenmask -l 0 - | swappy  -f - -o -"
-file=$HOME/Pictures/screenshot/$(date "+%F-%T").png
+file="$DIR/$(date "+%F-%T").png"
 img=""
 # 如果选择不是被取消的，则使用 grim 截图
 # 如果第一个参数的值是“edit”，则调用 swappy
@@ -12,12 +12,12 @@ if [ "$1" == "edit" ]; then
     if [ "$2" == "copy-name" ]; then
         result=$(ags request "screenshot \"$file\"")
         if [ "$result" == "" ]; then
-            swappy -f \"$file\" -o \"$file\" && wl-copy \"$file\"
+            swappy -f $file -o \"$file\" && wl-copy \"$file\"
         fi
     else
         result=$(ags request "screenshot \"$file\"")
         if [ "$result" == "" ]; then
-            swappy -f \"$file\" -o - | tee \"$file\" | wl-copy -t image/png
+            swappy -f $file -o - | tee \"$file\" | wl-copy -t image/png
         fi
     fi
 else
