a74f59e20fbb45983985cea769a2b6885c82e889
HumXC <Hum-XC@outlook.com>
2025-02-23 00:52:45 +0800

    fix wallpaper

--- hosts/theme.nix
+++ hosts/theme.nix
@@ -1,6 +1,8 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   stylix.enable = true;
   stylix.autoEnable = true;
+  services.hyprpaper.enable = lib.mkForce false; 
+  stylix.targets.hyprpaper.enable = lib.mkForce false;
   stylix.image = pkgs.fetchurl {
     url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
     sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
